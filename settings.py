4bfb8ac3779cf4981395f9c7b76dcd90f3a7bd7d
onlinejudge95 <onlinejudge95@gmail.com>
2020-04-22 18:43:38 +0530

    Adds env var

--- settings.py
+++ settings.py
@@ -9,3 +9,4 @@ ENV_FILE_PATH = BASE_PATH / ".env"
 load_dotenv(dotenv_path=ENV_FILE_PATH, verbose=True)
 
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
